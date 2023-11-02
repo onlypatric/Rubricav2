2e26ea460ee5c098ef15229036a87bdb4563ba16
Patric <patric.personal99@gmail.com>
2023-11-02 13:02:07 +0100

    types removed

--- MainApp.java
+++ MainApp.java
@@ -23,8 +23,6 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
